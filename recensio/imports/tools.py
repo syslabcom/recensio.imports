2253c1dc59ab7204d3b2e511facd9066e10d126c
gerken <gerken@51ee9ba4-e84d-4896-84de-354f1821c169>
2010-06-27 12:41:59 +0000

    We need a target url...
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.imports/trunk@5459 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/imports/tools.py
+++ recensio/imports/tools.py
@@ -1,5 +1,6 @@
 import sys, urllib
 
 def triggerPloneImportFromRecensio():
-    url = sys.argv[1]
-    urllib.urlopen(url)
+    trigger_url = sys.argv[1]
+    target_url = urllib.quote(sys.argv[2])
+    urllib.urlopen('%s?url=%s' % (trigger_url, target_url))
