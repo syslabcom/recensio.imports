b3205dd92d8f5ff22e5022406701804e06e87817
pilz <pilz@51ee9ba4-e84d-4896-84de-354f1821c169>
2012-04-19 21:34:43 +0000

    add commit after each pdf to reduce memory consumption
    git-svn-id: https://svn.syslab.com/svn/syslabcom/recensio.imports/trunk@9939 51ee9ba4-e84d-4896-84de-354f1821c169

--- recensio/imports/browser.py
+++ recensio/imports/browser.py
@@ -166,6 +166,7 @@ class MagazineImport(object):
             self.results.append(
                 {'name' : result_item.title,
                  'url' : result_item.absolute_url()})
+            transaction.commit()
         if self.errors:
             raise FrontendException()
 
