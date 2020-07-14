efd2784fce202eb320d227a88052778cf160f677
Manuel Reinhardt <reinhardt@syslab.com>
2020-07-14 08:58:32 +0200

    Catch KeyError in _sweepIndirectReferences (pyPdf). Refs SCR-441

--- recensio/imports/pdf_cut.py
+++ recensio/imports/pdf_cut.py
@@ -3,9 +3,19 @@ from StringIO import StringIO
 import pyPdf
 
 
+class RecensioPdfFileWriter(pyPdf.PdfFileWriter):
+    def _sweepIndirectReferences(self, externMap, data):
+        try:
+            return super(RecensioPdfFileWriter, self)._sweepIndirectReferences(
+                externMap, data
+            )
+        except KeyError:
+            return data
+
+
 def cutPDF(pdf, start, end):
     reader = pyPdf.PdfFileReader(pdf)
-    writer = pyPdf.PdfFileWriter()
+    writer = RecensioPdfFileWriter()
     inputPages = [reader.getPage(i) for i in range(reader.getNumPages())]
     pages = inputPages[int(start) - 1 : int(end)]
     for page in pages:
